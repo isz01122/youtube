fa1f457b37fc7d289e8045af0b6485a215d01d81
이준영 <junyoung@ijun-yeong-ui-MacBookPro.local>
2021-01-08 02:58:30 +0900

    add youtube service

--- src/app.jsx
+++ src/app.jsx
@@ -1,37 +1,29 @@
 import React, { useEffect, useState } from "react";
-import "./app.css";
+import styles from "./app.module.css";
 import VideoList from "./components/video_list/video_list";
+import SearchHeader from "./components/search_header/search_header";
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
 
-  useEffect(() => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
+  const search = (query) => {
+    youtube
+      .search(query) //
+      .then((videos) => setVideos(videos));
+  };
 
-    fetch(
-      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAFRCTSBDyr2xTy6ZkdnG6fBeBPMddkDfY",
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => setVideos(result.items))
-      .catch((error) => console.log("error", error));
+  useEffect(() => {
+    youtube
+      .mostPopular() //
+      .then((videos) => setVideos(videos));
   }, []);
 
-  return <VideoList videos={videos} />;
-
-  // <div>
-  //   <i className="fab fa-youtube"></i>
-  //   <div className="navbar">
-  //     <div className="navbar-icon">icon</div>
-  //     <div className="navbar-title">Youtube</div>
-  //     <input type="text" className="navbar-search" placeholder="Search" />
-  //     <button className="navbar-btn">검색</button>
-  //   </div>
-  //   <div className="list"></div>
-  // </div>
+  return (
+    <div className={styles.app}>
+      <SearchHeader onSearch={search} />
+      <VideoList videos={videos} />
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './app.jsx';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./app.jsx";
+import Youtube from "./service/youtube";
+
+//한번만 만들어서 전달한다!
+const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App youtube={youtube} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
