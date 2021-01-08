f86f2c622e15a0a416b1a888c642428a5784e992
이준영 <junyoung@ijun-yeong-ui-MacBookPro.local>
2021-01-09 00:29:57 +0900

    back to the list when searching

--- src/app.jsx
+++ src/app.jsx
@@ -2,11 +2,18 @@ import React, { useEffect, useState } from "react";
 import styles from "./app.module.css";
 import VideoList from "./components/video_list/video_list";
 import SearchHeader from "./components/search_header/search_header";
+import VideoDetail from "./components/video_detail/video_detail";
 
 function App({ youtube }) {
   const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const selectVideo = (video) => {
+    setSelectedVideo(video);
+  };
 
   const search = (query) => {
+    setSelectedVideo(null);
     youtube
       .search(query) //
       .then((videos) => setVideos(videos));
@@ -21,7 +28,21 @@ function App({ youtube }) {
   return (
     <div className={styles.app}>
       <SearchHeader onSearch={search} />
-      <VideoList videos={videos} />
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
+        )}
+
+        <div className={styles.list}>
+          <VideoList
+            videos={videos}
+            onVideoClick={selectVideo}
+            display={selectedVideo ? "list" : "grid"}
+          />
+        </div>
+      </section>
     </div>
   );
 }
--- src/service/youtube.js
+++ src/service/youtube.js
@@ -9,7 +9,7 @@ class Youtube {
 
   async mostPopular() {
     const response = await fetch(
-      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=26&regionCode=kr&key=${this.key}`,
+      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=5&regionCode=kr&key=${this.key}`,
       this.getRequestOptions
     );
     const result = await response.json();
@@ -18,7 +18,7 @@ class Youtube {
 
   async search(query) {
     const response = await fetch(
-      `https://youtube.googleapis.com/youtube/v3/search/?part=snippet&maxResults=26&q=${query}&type=video&key=${this.key}`,
+      `https://youtube.googleapis.com/youtube/v3/search/?part=snippet&maxResults=5&q=${query}&type=video&key=${this.key}`,
       this.getRequestOptions
     );
     const result = await response.json();
